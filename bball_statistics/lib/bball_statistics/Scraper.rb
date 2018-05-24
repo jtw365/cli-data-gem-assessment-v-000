0523f623cbf4275c31d34ee61bac070c15df67b8
Jonathan Williams <jtw365@live.missouristate.edu>
2018-05-24 19:06:35 +0000

    scrape players on toronto

--- bball_statistics/lib/bball_statistics/Scraper.rb
+++ bball_statistics/lib/bball_statistics/Scraper.rb
@@ -20,6 +20,19 @@ class Scraper
     # binding.pry
   end
 
+
+  def scrape_players
+    url = "https://www.basketball-reference.com/teams/TOR/2018.html"
+    doc = Nokogiri::HTML(open(url))
+    players = []
+    doc.css("div#div_roster tr").css("tr").css("a").each do |player|
+      puts player.text
+    end # player name
+    puts players
+
+  end
+
 end
 
-Scraper.new.scrape_teams
+# Scraper.new.scrape_teams
+Scraper.new.scrape_players
