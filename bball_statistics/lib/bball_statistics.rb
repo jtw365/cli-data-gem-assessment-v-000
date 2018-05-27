a211b32a89198019f740e3f322dab00d6a21e6d9
Jonathan Williams <jtw365@live.missouristate.edu>
2018-05-27 16:11:44 +0000

    initialize team added teams.rb to bball_statistics

--- bball_statistics/lib/bball_statistics.rb
+++ bball_statistics/lib/bball_statistics.rb
@@ -1,3 +1,4 @@
 require_relative "./bball_statistics/version"
 require_relative './bball_statistics/cli'
 require_relative './bball_statistics/Scraper'
+require_relative './bball_statistics/teams'
--- bball_statistics/lib/bball_statistics/teams.rb
+++ bball_statistics/lib/bball_statistics/teams.rb
@@ -1,4 +1,14 @@
 class Teams
 
+  attr_accessor :name
 
+  def initialize(name)
+    @name = name
+    @coach = coach
+    @record = record
+  end
+
+  def team
+
+  end
 end
