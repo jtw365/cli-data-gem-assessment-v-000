21fdc77bd7f1920bb339f181a2241545550cadcb
Jonathan Williams <jtw365@live.missouristate.edu>
2018-05-30 14:33:00 +0000

    added scrape_season_leaders method

--- bball_statistics/lib/bball_statistics/Scraper.rb
+++ bball_statistics/lib/bball_statistics/Scraper.rb
@@ -21,8 +21,11 @@ class Scraper
   end
 
   def scrape_team_data # toronto
-    url = "https://www.basketball-reference.com/teams/TOR/2018.html"
+    url = "https://www.basketball-reference.com/leagues/NBA_2018_leaders.html"
     doc = Nokogiri::HTML(open(url))
+    binding.pry
+    
+    
     team_record = doc.css("div#meta p")[2].text.strip.split(" ")[1].split(",").join() # "59-23,"
     coach_name = doc.css("div#meta p")[4].text.strip.split(" ")[1..2].join(" ") #" Dwane Casey"
     puts "Coach: #{coach_name}"
@@ -44,10 +47,22 @@ class Scraper
 
   end
 
+
+  def scrape_season_leaders
+    url = "https://www.basketball-reference.com/leagues/NBA_2018_leaders.html"
+    doc = Nokogiri::HTML(open(url))
+    binding.pry
+    puts "Assists Per Game Leaders"
+    doc.css("div#leaders_ast_per_g tr td").each do |a| #assist leader
+      puts a.text.strip
+    end
+  end
+  
 end
 
 # Scraper.new.scrape_teams
 puts "------------"
 # Scraper.new.scrape_team_data
 puts "------------"
-Scraper.new.scrape_players
+# Scraper.new.scrape_players
+Scraper.new.scrape_season_leaders
